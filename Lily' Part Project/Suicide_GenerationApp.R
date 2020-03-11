6685763f1ee9e0e8a1bef07808017920df5174cc
vincecast <vccastillo@ucdavis.edu>
2020-03-11 16:15:43 -0700

    Vincent's App

---
+++ Lily' Part Project/Suicide_GenerationApp.R
@@ -0,0 +1,59 @@
+if (!require("tidyverse")) install.packages('tidyverse')
+if (!require("shiny")) install.packages('shiny')
+if (!require("shinydashboard")) install.packages('shinydashboard')
+
+
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+
+ui <- 
+  dashboardPage(
+    dashboardHeader(title = "Suicide-Generation App"),
+    dashboardSidebar(disable = TRUE),
+    dashboardBody(
+      fluidRow(
+        box(title = "Plot Options", background = "maroon", solidHeader = TRUE, width = 2,
+            selectInput("generation", "Generation", choices = c("G.I. Generation", "Silent", "Boomers", "Generation X", "Millenials", "Generation Z"),
+                        selected = "G.I. Generation"),
+            selectInput("year", "Year", choices = c("1985", "1990", "1995", "2000", "2005", "2010", "2016"),
+                        selected = "1985"),
+            selectInput("sex", "Sex", choices = c("male", "female"))),
+        box(title = "Suicide Generation Plot", background = "maroon", solidHeader = TRUE, width = 10,
+            plotOutput("plot1", width = "1100px", height = "400px"))),
+      fluidRow(
+        box(title = "Plot Options", background = "fuchsia", solidHeader = TRUE, width = 2, 
+            selectInput("yearr", "Year", choices = c("1985", "1990", "1995", "2000", "2005", "2010", "2016"),
+                        selected = "1985"),
+            selectInput("agee", "Age", choices = c("5-14 years", "15-24 years", "25-34 years", "35-54 years", "55-74 years", "75+ years"), selected = "5-14 Years")),
+        box(title = "Suicide Sex Plot", background = "fuchsia", solidHeader = TRUE, width = 10,
+            plotOutput("plot2", width = "1100px", height = "400px"
+            )
+        )
+      )
+    )
+  )
+
+server <- function(input, output, session) { 
+  
+  
+  output$plot1 <- renderPlot({
+    
+    suicide %>%
+      filter(sex == input$sex, year == input$year, generation == input$generation) %>%
+      ggplot(aes(x = age, y = suicide_number, fill = age))+
+      geom_bar(stat = "identity")
+  })
+  output$plot2 <- renderPlot({
+    
+    suicide %>%
+      filter(year == input$yearr, age == input$agee) %>%
+      ggplot(aes(x = sex, y = suicide_number, fill = sex))+
+      geom_bar(stat = "identity")
+  })
+  
+  session$onSessionEnded(stopApp)
+  
+}
+
+shinyApp(ui, server)
